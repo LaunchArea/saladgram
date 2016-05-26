b9202d55d2dc7bdfdd31af13f6b8a27540f3d1cd
yns <hyunseok.yoon@tictocplanet.com>
2016-05-27 01:11:55 +0900

    HTTP, GSON hello world

--- android/staff/POS/app/src/main/java/com/saladgram/pos/MainActivity.java
+++ android/staff/POS/app/src/main/java/com/saladgram/pos/MainActivity.java
@@ -1,14 +1,71 @@
 package com.saladgram.pos;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    HashMap<String, ArrayList<HashMap<String, Object>>> mMenuList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new MenuListFetchTask().execute();
     }
 
+    class MenuListFetchTask extends AsyncTask<Void,Void,String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            OkHttpClient client = new OkHttpClient();
+
+
+            String url = "https://saladgram.com/api/menu_list.php";
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+
+            Response response = null;
+            try {
+                response = client.newCall(request).execute();
+                return response.body().string();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (result != null) {
+                mMenuList = new Gson().fromJson(result, new TypeToken<HashMap<String, ArrayList<HashMap<String, Object>>>>() {}.getType());
+                initializeUI();
+            }
+        }
+    }
+
+    private void initializeUI() {
+        findViewById(R.id.content_layout).setVisibility(View.VISIBLE);
+    }
 }
