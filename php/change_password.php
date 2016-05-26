b0dc1050c3e7412c43679a74eed48b613eb81ab0
Jinseok Kim <js@saladgram.com>
2016-05-26 02:26:59 +0000

    OPTIONS allowed for post preflight requests
    Signed-off-by: Jinseok Kim <js@saladgram.com>

--- php/change_password.php
+++ php/change_password.php
@@ -6,6 +6,10 @@ use \Firebase\JWT\ExpiredException;
 
 $method = $_SERVER['REQUEST_METHOD'];
 
+if ($method == "OPTIONS") {
+    return;
+}
+
 if ($method != "POST") {
     http_response_code(405); // Method Not Allowed
     return;
