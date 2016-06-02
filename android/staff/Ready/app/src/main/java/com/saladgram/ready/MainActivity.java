4447b11470fcfb99ea2e27a9d351e015d145ea03
yns <hyunseok.yoon@tictocplanet.com>
2016-06-03 00:13:47 +0900

    handle at-pickup payment_type

--- android/staff/Ready/app/src/main/java/com/saladgram/ready/MainActivity.java
+++ android/staff/Ready/app/src/main/java/com/saladgram/ready/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.support.annotation.IntegerRes;
 import android.support.design.widget.TabLayout;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AlertDialog;
@@ -60,15 +59,14 @@ public class MainActivity extends AppCompatActivity {
     private HashSet<String> mDeliverers = new HashSet<>(); //deliverer_id
 
     private TabLayout addrTabLayout;
-    private TabLayout typeTabLayout;
     private Button btnShipping;
     private Button btnReset;
-    private Order.OrderType mSelectedOrderType;
+
+    private Order.OrderType mSelectedOrderType = Order.OrderType.PICK_UP;
     private String mSelectedAddr;
-    private String[] addrList = {"All", "한화", "푸르지오", "아이파크"};
-    private List<String> typeList = new LinkedList<>();
-    private Map<String, Integer> mAddrCounter = new HashMap<>();
-    private Map<String, Integer> mTypeCounter = new HashMap<>();
+
+    private String[] tabLabelList = {"픽업", "한화", "푸르지오", "아이파크", "배달"};
+    private Map<String, Integer> mCounter = new HashMap<>();
     private Button btnDeliverer;
 
     @Override
@@ -76,7 +74,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initConstants();
         initView();
         initControl();
 
@@ -86,28 +83,14 @@ public class MainActivity extends AppCompatActivity {
         LocalBroadcastManager.getInstance(this).registerReceiver(mReceiver, filter);
     }
 
-    private void initConstants() {
-        typeList.add("All");
-        Order.OrderType[] values = Order.OrderType.values();
-        for (int i = 0; i < values.length; i++) {
-            typeList.add(values[i].name());
-        }
-    }
-
     private void initView() {
-        addrTabLayout = (TabLayout) findViewById(R.id.addr_tabs);
-        for(int i = 0; i < addrList.length; i++) {
-            TabLayout.Tab tab = addrTabLayout.newTab();
-            tab.setTag(addrList[i]);
-            tab.setText(addrList[i]);
-            addrTabLayout.addTab(tab);
-        }
+        addrTabLayout = (TabLayout) findViewById(R.id.tabs);
 
-        typeTabLayout = (TabLayout) findViewById(R.id.type_tabs);
-        for(int i = 0; i < typeList.size(); i++) {
-            String t = typeList.get(i);
-            TabLayout.Tab tab = typeTabLayout.newTab();
-            typeTabLayout.addTab(tab.setText(t).setTag(t));
+        for(int i = 0; i < tabLabelList.length; i++) {
+            TabLayout.Tab tab = addrTabLayout.newTab();
+            tab.setTag(tabLabelList[i]);
+            tab.setText(tabLabelList[i]);
+            addrTabLayout.addTab(tab);
         }
 
         lvOrders = (RecyclerView) findViewById(R.id.order_list);
@@ -120,7 +103,11 @@ public class MainActivity extends AppCompatActivity {
         btnShipping.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                postShipping();
+                if(mSelectedOrderType == Order.OrderType.PICK_UP) {
+                    postPickup();
+                } else {
+                    postShipping();
+                }
             }
         });
 
@@ -142,10 +129,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(View view, int position) {
                 int id = mOrderList.get(position).id;
-                if(mSelectedItems.contains(id)) {
-                    mSelectedItems.remove(id);
+                if (mSelectedOrderType == Order.OrderType.PICK_UP) {
+                    if (mSelectedItems.contains(id)) {
+                        mSelectedItems.remove(id);
+                    } else {
+                        mSelectedItems.clear();
+                        mSelectedItems.add(id);
+                    }
                 } else {
-                    mSelectedItems.add(id);
+                    if (mSelectedItems.contains(id)) {
+                        mSelectedItems.remove(id);
+                    } else {
+                        mSelectedItems.add(id);
+                    }
                 }
 
                 refreshUI();
@@ -169,41 +165,31 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 String tag = (String) tab.getTag();
-                if (tag == "All") {
+                if (tag == tabLabelList[0]) {
+                    mSelectedOrderType = Order.OrderType.PICK_UP;
+                    mSelectedAddr = null;
+                    mSelectedItems.clear();
+                } else if (tag == tabLabelList[tabLabelList.length-1]) {
+                    mSelectedOrderType = Order.OrderType.DELIVERY;
                     mSelectedAddr = null;
                 } else {
+                    mSelectedOrderType = Order.OrderType.DELIVERY;
                     mSelectedAddr = tag;
                 }
-
                 refreshUI();
             }
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-            }
-        });
-        typeTabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                String name = (String) tab.getTag();
-                if (name == "All") {
-                    mSelectedOrderType = null;
-                } else {
-                    mSelectedOrderType = Order.OrderType.valueOf(name);
+                String tag = (String) tab.getTag();
+                if (tag == tabLabelList[0]) {
+                    mSelectedItems.clear();
                 }
-                refreshUI();
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
             }
 
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
+
             }
         });
 
@@ -243,77 +229,125 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void refreshUI() {
-        mTypeCounter.clear();
-        mAddrCounter.clear();
+        mCounter.clear();
+        for (String label : tabLabelList) {
+            mCounter.put(label, 0);
+        }
 
         LinkedList<Order> fullList = new LinkedList<>(Service.orderList);
         LinkedList<Order> filteredList = new LinkedList<>();
-        for(Order order : fullList) {
-            String addr = order.addr;
-            for (int i = 0; i < addrList.length; i++) {
-                if(addr != null && addr.contains(addrList[i])) {
-                    if(!mAddrCounter.containsKey(addrList[i])) {
-                        mAddrCounter.put(addrList[i],0);
-                    }
-                    mAddrCounter.put(addrList[i],mAddrCounter.get(addrList[i]) + 1);
+        for (Order each : fullList) {
+            // filter
+            if (mSelectedOrderType == Order.OrderType.PICK_UP && each.orderType == Order.OrderType.PICK_UP) {
+                filteredList.add(each);
+            } else if (mSelectedOrderType == Order.OrderType.DELIVERY && each.orderType == Order.OrderType.DELIVERY) {
+                if (mSelectedAddr == null) {
+                    filteredList.add(each);
+                } else if (each.addr != null && each.addr.contains(mSelectedAddr)) {
+                    filteredList.add(each);
                 }
             }
+            // count
 
-            if (mSelectedAddr == null) {
-                if (mSelectedOrderType == null) {
-                    filteredList.add(order);
-                } else {
-                    if (order.orderType == mSelectedOrderType) {
-                        filteredList.add(order);
-                    }
-                }
-                String name = order.orderType.name();
-                if(!mTypeCounter.containsKey(name)) {
-                    mTypeCounter.put(name,0);
-                }
-                mTypeCounter.put(name,mTypeCounter.get(name) + 1);
-            } else {
-                if (order.addr != null && order.addr.contains(mSelectedAddr)) {
-                    if (mSelectedOrderType == null) {
-                        filteredList.add(order);
-                    } else {
-                        if (order.orderType == mSelectedOrderType) {
-                            filteredList.add(order);
+            if (each.orderType == Order.OrderType.PICK_UP) {
+                mCounter.put(tabLabelList[0], mCounter.get(tabLabelList[0]) + 1);
+            } else if (each.orderType == Order.OrderType.DELIVERY) {
+                if (each.addr != null) {
+                    for (String label : tabLabelList) {
+                        if (each.addr.contains(label)) {
+                            mCounter.put(label, mCounter.get(label) + 1);
+                            break;
                         }
                     }
-                    String name = order.orderType.name();
-                    if(!mTypeCounter.containsKey(name)) {
-                        mTypeCounter.put(name,0);
-                    }
-                    mTypeCounter.put(name,mTypeCounter.get(name) + 1);
+                    String key = tabLabelList[tabLabelList.length - 1];
+                    mCounter.put(key, mCounter.get(key) + 1);
                 }
             }
         }
-        int tcSum = 0;
-        for(int v : mTypeCounter.values()) {
-            tcSum += v;
-        }
-        mTypeCounter.put("All",tcSum);
-        mAddrCounter.put("All",fullList.size());
-
         mOrderList = new LinkedList<>(filteredList);
         mOrderAdapter.setList(mOrderList);
         mOrderAdapter.setSelectedIdList(mSelectedItems);
         mOrderAdapter.notifyDataSetChanged();
 
         btnShipping.setEnabled(mSelectedItems.size() > 0);
-        btnShipping.setText("SHIP " + mSelectedItems.size());
+        if(mSelectedOrderType == Order.OrderType.DELIVERY) {
+            btnShipping.setText("" + mSelectedItems.size() + " 개 배달나가기");
+        } else {
+            btnShipping.setText("픽업 주기");
+        }
 
         for(int i = 0; i < addrTabLayout.getTabCount(); i++) {
             TabLayout.Tab tab = addrTabLayout.getTabAt(i);
-            String key = addrList[i];
-            tab.setText(key + (mAddrCounter.containsKey(key) ? "\n" + mAddrCounter.get(key): ""));
+            String key = tabLabelList[i];
+            tab.setText(key + (mCounter.containsKey(key) ? "\n" + mCounter.get(key): ""));
         }
-        for(int i = 0; i < typeTabLayout.getTabCount(); i++) {
-            TabLayout.Tab tab = typeTabLayout.getTabAt(i);
-            String key = typeList.get(i);
-            tab.setText(key + (mTypeCounter.containsKey(key) ? "\n" + mTypeCounter.get(key): ""));
+    }
+
+    private void postPickup() {
+        Order order = null;
+        for (Integer id : mSelectedItems) {
+            for(Order each : mOrderList) {
+                if(each.id == id) {
+                    order = each;
+                    break;
+                }
+            }
         }
+        if (order == null) {
+            return;
+        }
+
+        if (order.paymentType == Order.PaymentType.AT_PICK_UP) {
+            choosePaymentTypeAndConfirmDone(order);
+        } else {
+            confirmDone(order, null);
+        }
+    }
+
+    private void choosePaymentTypeAndConfirmDone(final Order order) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setNeutralButton("카드", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                confirmDone(order, Order.PaymentType.CARD);
+            }
+        });
+        builder.setPositiveButton("현금", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                confirmDone(order, Order.PaymentType.CASH);
+            }
+        });
+        builder.setNegativeButton("현금영수증", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                confirmDone(order, Order.PaymentType.CASH_RECEIPT);
+            }
+        });
+        builder.show();
+    }
+
+    private void confirmDone(final Order order, final Order.PaymentType paymentType) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("done " + order.id + (paymentType == null ? "" : " with " + paymentType.name()));
+        builder.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                UpdateStatusTask task = new UpdateStatusTask(getActivity(), order, paymentType) {
+                    @Override
+                    protected void onPostExecute(Integer code) {
+                        super.onPostExecute(code);
+                        Toast.makeText(getActivity(), "" + (code == 200 ? "Success" : code), Toast.LENGTH_SHORT).show();
+                        if (code == 200) {
+                            mSelectedItems.clear();
+                            refreshUI();
+                        }
+                    }
+                };
+                task.execute();
+            }
+        });
+        builder.show();
     }
 
     private void postShipping() {
@@ -330,25 +364,6 @@ public class MainActivity extends AppCompatActivity {
         for (String each : mDeliverers) {
             arrayAdapter.add(each);
         }
-
-        builderSingle.setNegativeButton(
-                "배달자 불필요",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                        ShippingTask task = new ShippingTask(getActivity(), null, mSelectedItems) {
-                            @Override
-                            protected void onPostExecute(Integer integer) {
-                                super.onPostExecute(integer);
-                                Toast.makeText(getActivity(),"DONE " + mSelectedItems.size(),Toast.LENGTH_SHORT).show();
-                                mSelectedItems.clear();
-                                refreshUI();
-                            }
-                        };
-                        task.execute();
-                    }
-                });
 
         builderSingle.setAdapter(
                 arrayAdapter,
@@ -444,6 +459,80 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 return 200;
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return -1;
+        }
+    }
+
+    class UpdateStatusTask extends ProgressAsyncTask<Void, Void, Integer> {
+        final MediaType JSON
+                = MediaType.parse("application/json; charset=utf-8");
+        private final Order mOrder;
+        private final Order.PaymentType mPaymentType;
+
+        public UpdateStatusTask(Context context, Order order, Order.PaymentType paymentType) {
+            super(context);
+            this.mOrder = order;
+            this.mPaymentType = paymentType;
+        }
+
+        @Override
+        protected Integer doInBackground(Void... params) {
+            OkHttpClient client = new OkHttpClient();
+
+            try {
+                JSONObject signInJson = new JSONObject();
+
+                signInJson.put("id", "saladgram");
+                signInJson.put("password", "saladgram");
+                RequestBody body = RequestBody.create(JSON, signInJson.toString());
+
+                String url = "https://saladgram.com/api/sign_in.php";
+                Request request = new Request.Builder()
+                        .url(url)
+                        .post(body)
+                        .build();
+
+                Response response = null;
+                response = client.newCall(request).execute();
+                if (response.code() != 200) {
+                    return response.code();
+                }
+                String jwt = new JSONObject(response.body().string()).getString("jwt");
+
+                HashMap<String, Object> m = new HashMap<>();
+
+                m.put("order_id", mOrder.id);
+                m.put("paid", mOrder.json.getInt("actual_price"));
+                m.put("status", Order.Status.DONE.ordinal() + 1);
+                if (mPaymentType != null) {
+                    m.put("payment_type", mPaymentType.ordinal() + 1);
+                }
+
+                JSONObject json = new JSONObject(m);
+                body = RequestBody.create(JSON, json.toString());
+                Log.d("yns", json.toString(2));
+                url = "https://www.saladgram.com/api/update_order.php";
+                request = new Request.Builder()
+                        .header("jwt", jwt)
+                        .url(url)
+                        .post(body)
+                        .build();
+
+                response = client.newCall(request).execute();
+                Log.d("yns", response.body().string());
+                try {
+                    Service.update();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(getActivity(), e.getMessage() + " at order refresh", Toast.LENGTH_SHORT).show();
+                }
+                return response.code();
 
             } catch (JSONException e) {
                 e.printStackTrace();
