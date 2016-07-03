29902c6eedfffc7ad48ba1811ab8d06500c596d1
Jinseok Kim <js@saladgram.com>
2016-07-03 13:36:03 +0900

    deploy.sh for html files
    target is /usr/share/nginx/html/*test*
    
    Signed-off-by: Jinseok Kim <js@saladgram.com>

---
+++ html/deploy.sh
@@ -0,0 +1,8 @@
+TARGET=/usr/share/nginx/html/test
+
+sudo cp *.html $TARGET
+sudo cp -R assets $TARGET
+sudo cp -R css $TARGET
+sudo cp -R img $TARGET
+sudo cp -R js $TARGET
+sudo cp -R templates $TARGET
