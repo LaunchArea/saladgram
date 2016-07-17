033190055ad7ad97dbd021cf7d8ee39a35512b4e
Jinseok Kim <js@saladgram.com>
2016-07-17 17:14:18 +0900

    delete iframe on close.php

--- inicis/inicis/close.php
+++ inicis/inicis/close.php
@@ -1,1 +1,16 @@
-<script language="javascript" type="text/javascript" src="https://stdpay.inicis.com/stdjs/INIStdPay_close.js" charset="UTF-8"></script>
+<!DOCTYPE html>
+<html>
+<head>
+<script language="javascript" type="text/javascript" charset="UTF-8">
+    function script() {
+        console.log("jack");
+        var el = top.document.getElementsByTagName('iframe');
+        console.log(el[0]);
+        console.log(el[0].parentNode);
+        el[0].parentNode.removeChild(el[0]);
+    }
+</script>
+</head>
+<body onload="script();">
+</body>
+</html>
